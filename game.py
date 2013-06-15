08da0286857fabb5ea62965578fe039f58223a0c
mebusw <mebusw@163.com>
2013-06-15 23:37:30 +0800

    implement special rules of Merry, Lagolas, Orc, Frodo, Pippin, Warg. Introduce game state.

--- game.py
+++ game.py
@@ -19,11 +19,12 @@ class Game(object):
     
     def player1Move(self, spawn, dest):
         self.spawns[spawn] = dest
+        self.resolve(dest, spawn)
 
     def spawnsAt(self, tile):
         return [spawn for spawn in self.spawns if self.spawns[spawn] == tile]
 
-    def _getWeakSpawnsAt(self, spawns, tile):
+    def _getWeakSpawns(self, spawns):
         weaks = []
         if spawns[0].isStrongerThan(spawns[1]):
             weaks.append(spawns[1])
@@ -31,10 +32,52 @@ class Game(object):
             weaks.append(spawns[0])
         return weaks
 
-    def resolve(self, tile):
-        spawns = self.spawnsAt(tile)
-        for weak in self._getWeakSpawnsAt(spawns, tile):
-            del self.spawns[weak]
+    def resolve(self, tile, attacker):
+        fightingSpawns = self.spawnsAt(tile)
+
+        if attacker != Spawns.Warg:
+
+            if Spawns.Pippin in fightingSpawns and attacker == Spawns.Pippin:
+                self._state = 'if_retreat_s'
+                return
+
+            if Spawns.Frodo in fightingSpawns and attacker != Spawns.Frodo:
+                self._state = 'if_retreat_s'
+                return
+
+            for rule in [self.rule_Orc_kills_Legolas_immediately, 
+                            self.rule_Merry_kills_WitchKing_immediately,
+                            self.rule_Legolas_kills_FlyingNazgul_immediately,
+                            ]:
+                victims = rule(fightingSpawns, attacker)
+                if victims:
+                    for victim in victims:
+                        del self.spawns[victim]
+                    return
+
+        for victim in self.rule_basic(fightingSpawns):
+            del self.spawns[victim]
+
+    def rule_basic(self, fightingSpawns, attacker=None):
+        return self._getWeakSpawns(fightingSpawns)  
+
+    def rule_Merry_kills_WitchKing_immediately(self, fightingSpawns, attacker=None):
+        if Spawns.Merry in fightingSpawns and Spawns.WitchKing in fightingSpawns:
+            return [Spawns.WitchKing]
+
+
+    def rule_Legolas_kills_FlyingNazgul_immediately(self, fightingSpawns, attacker=None):
+        if Spawns.Legolas in fightingSpawns and Spawns.FlyingNazgul in fightingSpawns:
+            return [Spawns.FlyingNazgul]
+
+    def rule_Orc_kills_Legolas_immediately(self, fightingSpawns, attacker=None):
+        if Spawns.Legolas in fightingSpawns and \
+            Spawns.Orcs in fightingSpawns and attacker == Spawns.Orcs:
+            return [Spawns.Legolas]
+
+    def getState(self):
+        return self._state
+
 
 class Spawn(object):
     def __init__(self, name, strengh, camp):
@@ -44,3 +87,20 @@ class Spawn(object):
                 
     def isStrongerThan(self, other):
         return self.strengh >= other.strengh
+
+    def __str__(self):
+        return self.name
+
+
+class Spawns:
+    Frodo = Spawn('Frodo',1 , 'Light')
+    Sam = Spawn('Sam', 2, 'Light')
+    Merry = Spawn('Merry', 2, 'Light')
+    Legolas = Spawn('Legolas', 3, 'Light')
+    Pippin = Spawn('Pippin', 1, 'Light')
+
+    Shelob = Spawn('Shelob', 5, 'Dark')
+    Warg = Spawn('Warg', 2, 'Dark')
+    WitchKing = Spawn('WitchKing', 5, 'Dark')
+    FlyingNazgul = Spawn('FlyingNazgul', 3, 'Dark')
+    Orcs = Spawn('Orcs', 3, 'Dark')   
--- gameTest.py
+++ gameTest.py
@@ -6,48 +6,88 @@ Created on May 20, 2013
 import unittest
 from game import *
 
-class Test(unittest.TestCase):
+
+
+class TestGameRules(unittest.TestCase):
 
 
     def setUp(self):
-        pass
+        self.game = Game()
 
     def tearDown(self):
         pass
 
-    def test_player1_move_spawn(self):
-        game = Game()
-        spawn = Spawn('Frodo',1 , 'Expedit') 
-        game.setSpawn(spawn, "Hollin")
-        game.player1Move(spawn, dest = "Moria")
-
-        self.assertIn(spawn, game.spawnsAt("Moria"))
-        self.assertNotIn(spawn, game.spawnsAt("Hollin"))
-
 
     def test_player2_meet_player1_then_stronger_win(self):
-        game = Game()
-        spawn1 = Spawn('Sam', 2, 'Expedit')
-        spawn2 = Spawn('Shelob', 5, 'Sauron')
-        game.setSpawn(spawn1, "Moria")
-        game.setSpawn(spawn2, "Moria")
-        game.resolve("Moria")
+        self.game.setSpawn(Spawns.Sam, "Moria")
+        self.game.setSpawn(Spawns.Shelob, "MHollinoria")
+        self.game.player1Move(Spawns.Shelob, "Moria")
 
-        self.assertIn(spawn2, game.spawnsAt("Moria"))
-        self.assertNotIn(spawn1, game.spawnsAt("Moria"))
+        self.assertIn(Spawns.Shelob, self.game.spawnsAt("Moria"))
+        self.assertNotIn(Spawns.Sam, self.game.spawnsAt("Moria"))
 
     def test_player2_meet_player1_then_both_die(self):
-        game = Game()
-        spawn1 = Spawn('Sam', 2, 'Expedit')
-        spawn2 = Spawn('Warg', 2, 'Sauron')
-        game.setSpawn(spawn1, "Moria")
-        game.setSpawn(spawn2, "Moria")
-        game.resolve("Moria")
+        self.game.setSpawn(Spawns.Sam, "Moria")
+        self.game.setSpawn(Spawns.Warg, "Hollin")
+        self.game.player1Move(Spawns.Warg, "Moria")
 
-        self.assertNotIn(spawn2, game.spawnsAt("Moria"))
-        self.assertNotIn(spawn1, game.spawnsAt("Moria"))
+        self.assertNotIn(Spawns.Warg, self.game.spawnsAt("Moria"))
+        self.assertNotIn(Spawns.Sam, self.game.spawnsAt("Moria"))
 
-        
+    def test_Merry_defeat_WitchKing_immediately(self):
+        self.game.setSpawn(Spawns.Merry, "Moria")
+        self.game.setSpawn(Spawns.WitchKing, "Hollin")
+        self.game.player1Move(Spawns.WitchKing, "Moria")
+
+        self.assertIn(Spawns.Merry, self.game.spawnsAt("Moria"))
+        self.assertIn(Spawns.WitchKing, self.game.spawnsAt("Moria"))
+
+    def test_Legolas_defeat_FlyingNazgul_immediately(self):
+        self.game.setSpawn(Spawns.Legolas, "Moria")
+        self.game.setSpawn(Spawns.FlyingNazgul, "Hollin")
+        self.game.player1Move(Spawns.FlyingNazgul, "Moria")
+
+        self.assertIn(Spawns.Legolas, self.game.spawnsAt("Moria"))
+        self.assertNotIn(Spawns.FlyingNazgul, self.game.spawnsAt("Moria"))
+
+    def test_Orc_defeat_Legolas_immediately(self):
+        self.game.setSpawn(Spawns.Legolas, "Moria")
+        self.game.setSpawn(Spawns.Orcs, "Hollin")
+        self.game.player1Move(Spawns.Orcs, "Moria")
+
+        self.assertIn(Spawns.Orcs, self.game.spawnsAt("Moria"))
+        self.assertNotIn(Spawns.Legolas, self.game.spawnsAt("Moria"))
+
+    def test_Legolas_attack_Orc_and_both_die(self):
+        self.game.setSpawn(Spawns.Orcs, "Moria")
+        self.game.setSpawn(Spawns.Legolas, "Hollin")
+        self.game.player1Move(Spawns.Legolas, "Moria")
+
+        self.assertNotIn(Spawns.Orcs, self.game.spawnsAt("Moria"))
+        self.assertNotIn(Spawns.Legolas, self.game.spawnsAt("Moria"))
+
+    def test_Warg_elimates_ability_of_opponent(self):
+        self.game.setSpawn(Spawns.Frodo, "Moria")
+        self.game.setSpawn(Spawns.Warg, "Hollin")
+        self.game.player1Move(Spawns.Warg, "Moria")
+
+        self.assertIn(Spawns.Warg, self.game.spawnsAt("Moria"))
+        self.assertNotIn(Spawns.Frodo, self.game.spawnsAt("Moria"))
+
+    def test_Frodo_can_retreat_when_attacked(self):
+        self.game.setSpawn(Spawns.Frodo, "Moria")
+        self.game.setSpawn(Spawns.Orcs, "Hollin")
+        self.game.player1Move(Spawns.Orcs, "Moria")
+
+        self.assertEqual(self.game.getState(), "if_retreat_s")
+
+    def test_Pippin_can_retreat_when_attacking(self):
+        self.game.setSpawn(Spawns.Pippin, "Hollin")
+        self.game.setSpawn(Spawns.Orcs, "Moria")
+        self.game.player1Move(Spawns.Pippin, "Moria")
+
+        self.assertEqual(self.game.getState(), "if_retreat_s")
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
